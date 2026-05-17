94de9f34ccb248a3df8c86214cbeef232d8293a5
Chockalingam Alagappan <chockalingamfullstackdev@gmail.com>
2026-05-18 00:10:46 +0530

    login and signup method

--- taskflow/manager/src/main/java/com/work/taskflow/manager/controller/LoginController.java
+++ taskflow/manager/src/main/java/com/work/taskflow/manager/controller/LoginController.java
@@ -3,28 +3,29 @@ package com.work.taskflow.manager.controller;
 import com.work.taskflow.manager.dao.UserDao;
 import com.work.taskflow.manager.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginController {
 
-    @Autowired
-    UserDao userDao;
+  @Autowired UserDao userDao;
 
- @GetMapping ("/hello")
-public String testMethod(){
-    return "hello world";
-}
 
-@GetMapping ("/hello/{userName}/{password}")
-public String login(@PathVariable String userName, @PathVariable  String password){
+
+  @GetMapping("/hello")
+  public String login(@RequestParam String userName, @RequestParam String password) {
     User user = userDao.findByUserName(userName);
-    if(user!=null){
-        return (user.getPassword().equals(password)?"password match - login sucess":"wrong password");
+    if (user != null) {
+      return (user.getPassword().equals(password)
+          ? "password match - login sucess"
+          : "wrong password");
     }
     return "user doesnt exist";
-}
+  }
 
+  @PostMapping("/hello/addUser")
+  public String signUp(@RequestBody User user) {
+    User user1 = userDao.save(user);
+    return user1.toString();
+  }
 }
